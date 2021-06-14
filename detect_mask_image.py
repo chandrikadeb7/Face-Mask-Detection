47a969554aa40ad5c04415b21e077fbd1f98a73d
EasonHuang-tw <eason27271563.me05@nctu.edu.tw>
2021-06-15 00:07:41 +0800

    use web_cam

--- detect_mask_image.py
+++ detect_mask_image.py
@@ -37,68 +37,84 @@ def mask_image():
 
 	# load the input image from disk, clone it, and grab the image spatial
 	# dimensions
-	image = cv2.imread(args["image"])
-	orig = image.copy()
-	(h, w) = image.shape[:2]
+	default_path = 'images/'
+	image_name = [default_path+'pic1.jpeg',default_path+'pic2.jpg',default_path+'pic3.jpg']
+	x = 0
+	cap = cv2.VideoCapture(0)
+	while(1):
+		#image = cv2.imread(args["image"])
+		#image = cv2.imread(image_name[x])
+		#x=x+1
+		#x%=(len(image_name))
+		ret, image = cap.read()
+		orig = image.copy()
+		(h, w) = image.shape[:2]
 
-	# construct a blob from the image
-	blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
-		(104.0, 177.0, 123.0))
+		# construct a blob from the image
+		blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300),
+			(104.0, 177.0, 123.0))
 
-	# pass the blob through the network and obtain the face detections
-	print("[INFO] computing face detections...")
-	net.setInput(blob)
-	detections = net.forward()
+		# pass the blob through the network and obtain the face detections
+		print("[INFO] computing face detections...")
+		net.setInput(blob)
+		detections = net.forward()
 
-	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
-		confidence = detections[0, 0, i, 2]
+		# loop over the detections
+	
+		for i in range(0, detections.shape[2]):
+			# extract the confidence (i.e., probability) associated with
+			# the detection
+			confidence = detections[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+			# filter out weak detections by ensuring the confidence is
+			# greater than the minimum confidence
+			if confidence > args["confidence"]:
+				# compute the (x, y)-coordinates of the bounding box for
+				# the object
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
-			(startX, startY) = (max(0, startX), max(0, startY))
-			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+				# ensure the bounding boxes fall within the dimensions of
+				# the frame
+				(startX, startY) = (max(0, startX), max(0, startY))
+				(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
-			face = image[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
-			face = np.expand_dims(face, axis=0)
+				# extract the face ROI, convert it from BGR to RGB channel
+				# ordering, resize it to 224x224, and preprocess it
+				face = image[startY:endY, startX:endX]
+				if(face.any()):
+					face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+					face = cv2.resize(face, (224, 224))
+					face = img_to_array(face)
+					face = preprocess_input(face)
+					face = np.expand_dims(face, axis=0)
 
-			# pass the face through the model to determine if the face
-			# has a mask or not
-			(mask, withoutMask) = model.predict(face)[0]
+					# pass the face through the model to determine if the face
+					# has a mask or not
+					(mask, withoutMask) = model.predict(face)[0]
 
-			# determine the class label and color we'll use to draw
-			# the bounding box and text
-			label = "Mask" if mask > withoutMask else "No Mask"
-			color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+					# determine the class label and color we'll use to draw
+					# the bounding box and text
+					label = "Mask" if mask > withoutMask else "No Mask"
+					color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
-			# include the probability in the label
-			label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+					# include the probability in the label
+					label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-			# display the label and bounding box rectangle on the output
-			# frame
-			cv2.putText(image, label, (startX, startY - 10),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-			cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+					# display the label and bounding box rectangle on the output
+					# frame
+					cv2.putText(image, label, (startX, startY - 10),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+					cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
+				else:
+					pass
 
-	# show the output image
-	cv2.imshow("Output", image)
-	cv2.waitKey(0)
+		# show the output image
+		cv2.imshow("Output", image)
+		if cv2.waitKey(1) == ord('q'):
+			break
+		#cv2.waitKey(0)
+		#print('haha!')
 	
 if __name__ == "__main__":
 	mask_image()
