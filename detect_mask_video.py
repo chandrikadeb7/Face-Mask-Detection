1d66aeaad3fa8702a97bfa7aad4676f56d2822a1
ritikchinu <75379885+ritikchinu@users.noreply.github.com>
2021-08-27 16:47:35 +0530

    Frame crash issue resolved
    The issue involved empty frame being captured by caffe based face detection algorithm when we approach close to the camera lens. I have added necessary conditional statement before pre-processing the facial frame to avoid the framework from crashing while executing pre-processing methods in empty frame.

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -52,15 +52,16 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 			# extract the face ROI, convert it from BGR to RGB channel
 			# ordering, resize it to 224x224, and preprocess it
 			face = frame[startY:endY, startX:endX]
-			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-			face = cv2.resize(face, (224, 224))
-			face = img_to_array(face)
-			face = preprocess_input(face)
+			if face.any():
+				face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+				face = cv2.resize(face, (224, 224))
+				face = img_to_array(face)
+				face = preprocess_input(face)
 
-			# add the face and bounding boxes to their respective
-			# lists
-			faces.append(face)
-			locs.append((startX, startY, endX, endY))
+				# add the face and bounding boxes to their respective
+				# lists
+				faces.append(face)
+				locs.append((startX, startY, endX, endY))
 
 	# only make a predictions if at least one face was detected
 	if len(faces) > 0:
